190c8b285bfc39afee46b544dd1d6e0c40735f5c
Jake Elwes <me@jakeelwes.com>
2021-08-17 15:58:18 +0100

    closed befriend

--- src/pages/befriending-closed.tsx
+++ src/pages/befriending-closed.tsx
@@ -3,9 +3,12 @@ import Helmet from 'react-helmet';
 
 import logoWide from '../img/logo-wide.png';
 
-import { Div, Txt } from '../core/elements';
+import { Div, Txt, Hover, Icon } from '../core/elements';
 import Layout from '../core/Layout';
-import styles from '../core/styles';
+import contactIcon from '../img/icons/contact.png';
+import styles, { colors, icons } from '../core/styles';
+
+
 
 export default () => (
   <Layout>
@@ -27,9 +30,59 @@ export default () => (
         We believe this to be a humanitarian response to the dreadful situation in Afghanistan but as we are a small organisation making matches between refugees and residents in London, we cannot process too many registrations at once.
         <br /><br />
 
-
-        Thank you for your interest and please feel free to get in contact with us directly if you are keen to befriend in the future info@hostnation.org.uk.
+        Thank you for your interest and please feel free to get in contact with us directly if you are keen to befriend in the future.
       </Txt>
     </Div>
+    <Div style={{ spacing: 35 }}>
+        <Div style={{ spacing: 15 }}>
+          <img src={contactIcon} style={{ width: 100, margin: '0 auto' }} />
+          <Hover
+            style={{
+              ...styles.subtitle,
+              color: colors.purple,
+              hover: { color: colors.purpleDark },
+            }}
+          >
+            {({ hoverProps, style }) => (
+              <a href="mailto:info@hostnation.org.uk">
+                <Txt {...hoverProps} style={style}>
+                  info@hostnation.org.uk
+                </Txt>
+              </a>
+            )}
+          </Hover>
+        </Div>
+        <Div style={{ layout: 'bar', spacing: 40, margin: '0 auto' }}>
+          {[
+            {
+              link: 'https://www.facebook.com/HostNationUK',
+              icon: icons.fbThin,
+            },
+            {
+              link: 'https://twitter.com/hostnationuk',
+              icon: icons.twitter,
+            },
+          ].map(({ link, icon }, i) => (
+            <Hover
+              style={{
+                display: 'block',
+                padding: 12,
+                margin: -5,
+                background: colors.purple,
+                borderRadius: 100,
+                hover: { background: colors.purpleDark },
+              }}
+              key={i}
+            >
+              {({ hoverProps, style }) => (
+                <a href={link} target="_blank" {...hoverProps} style={style}>
+                  <Icon {...icon} style={{ color: 'white', fontSize: 24 }} />
+                </a>
+              )}
+            </Hover>
+          ))}
+        </Div>
+        <Div></Div>
+      </Div>
   </Layout>
 );
